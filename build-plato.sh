d755ef087c8d5d530f0bc7de64045dae04cc6031
Matthew Spellings <mspells@umich.edu>
2018-05-07 16:03:56 -0400

    Also tag latest on plato-tests image

--- build-plato.sh
+++ build-plato.sh
@@ -11,6 +11,10 @@ docker tag "${core_tag}" "${latest_tag}"
 docker push "${core_tag}"
 docker push "${latest_tag}"
 
+TAG=$(date +%Y%m%d)
 tests_tag="${OWNER}/plato-tests:${TAG}"
+latest_tag="${OWNER}/plato-tests:latest"
 docker build -f plato-tests/Dockerfile -t "${tests_tag}" .
+docker tag "${tests_tag}" "${latest_tag}"
 docker push "${tests_tag}"
+docker push "${latest_tag}"
