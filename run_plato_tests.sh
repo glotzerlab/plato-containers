4fbb1403821024a5530322af487eee8bbd8a7c04
Matthew Spellings <mspells@umich.edu>
2018-05-07 17:41:58 -0400

    Make test artifact dir

--- run_plato_tests.sh
+++ run_plato_tests.sh
@@ -1,8 +1,14 @@
+set -e
+
 cd
+
+mkdir -p "${PLATO_TEST_ARTIFACT_DIR}"
 
 git clone https://bitbucket.org/glotzer/plato plato
 cd plato
 
-python3 -m pip install . vispy pyglet --user
+# use global numpy/scipy
+python3 -m pip install --no-deps --user .
+python3 -m pip install vispy pyglet --user
 cd test
 python3 -m nose
