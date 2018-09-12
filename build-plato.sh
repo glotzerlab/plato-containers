c2f8304d4e21a175f6c29dc6076ddc19fb9233e6
Matthew Spellings <mspells@umich.edu>
2018-09-12 14:09:13 -0400

    Base images on debian instead

--- build-plato.sh
+++ build-plato.sh
@@ -4,17 +4,11 @@ set -e
 OWNER=mspells
 TAG=$(date +%Y%m%d)
 
-core_tag="${OWNER}/plato-core:${TAG}"
-latest_tag="${OWNER}/plato-core:latest"
-docker build -f plato-core/Dockerfile -t "${core_tag}" .
-docker tag "${core_tag}" "${latest_tag}"
-docker push "${core_tag}"
-docker push "${latest_tag}"
-
-TAG=$(date +%Y%m%d)
-tests_tag="${OWNER}/plato-tests:${TAG}"
-latest_tag="${OWNER}/plato-tests:latest"
-docker build -f plato-tests/Dockerfile -t "${tests_tag}" .
-docker tag "${tests_tag}" "${latest_tag}"
-docker push "${tests_tag}"
-docker push "${latest_tag}"
+for package in plato-core plato-tests;do
+  core_tag="${OWNER}/${package}:${TAG}"
+  latest_tag="${OWNER}/${package}:latest"
+  docker build -f ${package}/Dockerfile -t "${core_tag}" .
+  docker tag "${core_tag}" "${latest_tag}"
+  docker push "${core_tag}"
+  docker push "${latest_tag}"
+done
